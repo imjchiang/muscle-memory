5451f8a6d32b27e509e9403adbc6a4c8f913fc46
imjchiang <jchiang296@gmail.com>
2020-07-17 14:09:15 -0700

    [JS] add variables and print onn term

---
+++ app.js
@@ -0,0 +1,10 @@
+var myString = "This is a string";
+var myNumber = 5;
+var myArray = [1, 2, 3, 4, 5];
+var myBoolean = true;
+const myObject = {
+    propertyString: "This is another string",
+    propertyNumber: 22,
+    propertyArray: [9, 8, 7, 6, 5],
+    propertyBoolean: false
+};
